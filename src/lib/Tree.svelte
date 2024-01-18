6c6930109ef207fe4542efc4614d9bdf5e5f7f89
tarneo <tarneo@tarneo.fr>
2024-01-18 09:47:40 +0100

    Half working tree

---
+++ src/lib/Tree.svelte
@@ -0,0 +1,76 @@
+<div class="tree">
+  <ul>
+    <li>
+      <div>+</div>
+      <ul>
+        <li>
+          <div>+</div>
+          <ul>
+            <li>
+              2
+            </li>
+            <li>
+              4
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div>-</div>
+          <ul>
+            <li>
+              <div>-</div>
+              <ul>
+                <li>
+                  2
+                </li>
+                <li>
+                  4
+                </li>
+              </ul>
+            </li>
+            <li>
+              4
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
+
+<style>
+
+.tree {
+  font-size: 10rem;
+}
+
+.tree ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+  display: flex;
+}
+
+.tree>ul>li ul {
+  position: relative;
+}
+
+.tree>ul>li ul:before {
+  content: '';
+  background-color: red;
+  position: absolute;
+  top: 0;
+  left: 20%;
+  height: 1px;
+  width: 60%;
+}
+
+.tree li {
+  margin-right: 20px;
+}
+
+.tree li:last-child {
+  margin-right: 0;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Tree from '$lib/Tree.svelte';
+</script>
+
+<h1>Welcome Guillaume</h1>
+<p>Bonjour <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Tree />
